8c766d7b57731363ab741faf6a1a2a33a9bc7836
Ruchi Jindal <ruchi@knoldus.com>
2012-09-12 16:28:36 +0530

    Update rational Class

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -9,28 +9,17 @@ class Rational(n: Int, d: Int) {
       this.denom * that.denom)
 }
 
-//class Rational(n: Int, d: Int) {
-//  require(d != 0)
-//  val numer: Int = n
-//  val denom: Int = d
-//  override def toString = numer + "/" + denom
-//}
-//
-//class Add {
-//  def add(r1:Rational, r2:Rational): Rational =
-//    {
-//      new Rational(r1.numer * r2.denom + r2.numer * r1.denom ,r1.denom * r2.denom)
-//    }
-//}
 
 object Main extends App {
-  val oneHalf = new Rational(1, 2)
+  val oneHalf = new Rational(1, 2)  
   println("oneHalf: " + oneHalf)
+  
   val twoThirds = new Rational(2, 3)
-  println("twoThirds: " + twoThirds)
+  println("twoThirds: " + twoThirds)\
+  
   println("Result:  " + (oneHalf add twoThirds))
   
-  println("Result:  " + (oneHalf.add(twoThirds)))
-  //println("Result : "+ (new Add).add(oneHalf,twoThirds))
+  
+ 
 
 }
